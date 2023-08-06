19de716424e00777c356370a09cd5deb5d4c91d0
JJChong777 <jiejun815@gmail.com>
2023-08-06 13:41:36 +0800

    final

--- 17-the-wild-oasis/the-wild-oasis/src/pages/Cabins.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/pages/Cabins.jsx
@@ -1,19 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
+import AddCabin from "../features/cabins/AddCabin";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => console.log(data));
-  }, []);
-
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img src="https://zcagyinqdudrczpwlxfq.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg" />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <CabinTableOperations />
+      </Row>
+
+      <Row>
+        <CabinTable />
+        <AddCabin />
+      </Row>
+    </>
   );
 }
 
--- 17-the-wild-oasis/the-wild-oasis/src/services/supabase.js
+++ 17-the-wild-oasis/the-wild-oasis/src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://zcagyinqdudrczpwlxfq.supabase.co";
+export const supabaseUrl = "https://zcagyinqdudrczpwlxfq.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjYWd5aW5xZHVkcmN6cHdseGZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk4MjIxMjYsImV4cCI6MjAwNTM5ODEyNn0.uQw5OuUpJ3a1MmlTAMUxwp-d5pg2SMP9S4j_SZGej5I";
 const supabase = createClient(supabaseUrl, supabaseKey);
